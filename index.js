5d555f5916efccdcfa6f19ba3572f9cf3f312a31
shinespark <shnsprk@gmail.com>
2019-11-28 01:51:00 +0900

    最小限で動作するコードに整理

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const moment = require('moment');
 const puppeteer = require('puppeteer');
 const readline = require('readline');
 const {google} = require('googleapis');
-const {promisify} = require('util');
 
 const SBISEC = {
   "loginUrl": "https://site1.sbisec.co.jp/ETGate/",
   "messageBoxUrl": "https://site1.sbisec.co.jp/ETGate/?_ControlID=WPLETmbR001Control&_PageID=WPLETmbR001Rsub15&_DataStoreID=DSWPLETmbR001Control&_ActionID=DefaultAID&category_id=03&message_filter_value=2"
 }
 
+// https://developers.google.com/gmail/api/quickstart/nodejs#step_3_set_up_the_sample
+// https://github.com/googleapis/google-api-nodejs-client/blob/master/samples/gmail/send.js
 const SCOPES = ['https://www.googleapis.com/auth/gmail.send'];
 const TOKEN_PATH = 'token.json';
 let auth;
 fs.readFile('credentials.json', (err, content) => {
-  console.log(JSON.parse(content));
   if (err) return console.log('Error loading client secret file:', err);
   const {client_secret, client_id, redirect_uris} = JSON.parse(content).installed;
   const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
@@ -23,15 +23,9 @@ fs.readFile('credentials.json', (err, content) => {
   fs.readFile(TOKEN_PATH, (err, token) => {
     if (err) return getNewToken(oAuth2Client, callback);
     oAuth2Client.setCredentials(JSON.parse(token));
-    console.log(oAuth2Client);
-    // callback(oAuth2Client, from, to, title, body);
-    // callback(oAuth2Client);
     auth = oAuth2Client;
   });
 });
-
-
-const date = moment().format(moment.HTML5_FMT.DATETIME_LOCAL_MS);
 
 async function login(page) {
   await page.goto(SBISEC.loginUrl, {waitUntil: 'networkidle2'});
@@ -51,7 +45,7 @@ async function getUnreadMessages(page) {
     for (let i = 0; unreadMessages.length > i; i++) {
       list.push({
         href: unreadMessages[i].href,
-        text: unreadMessages[i].textContent
+        text: unreadMessages[i].textContent.trim()
       })
     }
     return list
@@ -60,23 +54,21 @@ async function getUnreadMessages(page) {
   return result
 }
 
-// async function message2mail(page, url, oAuth2Client) {
 async function message2mail(page, url) {
   await page.goto(url, {waitUntil: 'networkidle2'});
 
-  const subjectSelector = 'form > table:nth-of-type(2)';
+  const subjectSelector = 'form > table:nth-of-type(1) > tbody > tr:last-child';
   const bodySelector = 'form > table:nth-of-type(3)';
   const submitSelector = 'form > table:nth-of-type(4) input[type="submit"]';
   const subject = await page.$eval(subjectSelector, item => {
-    return item.textContent;
+    return item.textContent.trim();
   });
   const body = await page.$eval(bodySelector, item => {
-    return item.textContent;
+    return item.innerHTML;
   });
 
-  // const res = await sendMail(oAuth2Client, subject, body);
   const res = await sendMail(subject, body);
-  await console.log(res);
+  console.log(res.status);
   if (res && res.status === 200) {
     // await page.click(submitSelector);
   }
@@ -87,7 +79,7 @@ async function sendMail(subject, body) {
   const utf8Subject = `=?utf-8?B?${Buffer.from(subject).toString('base64')}?=`;
   const messageParts = [
     `From: sbisec-message2mail <${process.env.FROM}>`,
-    `To: my own <${process.env.TO}>`,
+    `To: Yourself <${process.env.TO}>`,
     'Content-Type: text/html; charset=utf-8',
     'Content-Transfer-Encoding: 7bit',
     'MIME-Version: 1.0',
@@ -105,47 +97,11 @@ async function sendMail(subject, body) {
     .replace(/=+$/, '');
 
   const gmail = google.gmail({version: 'v1', auth: auth});
-  console.log(gmail)
-  gmail.users.messages.send({
+  return gmail.users.messages.send({
     userId: 'me',
     requestBody: {
       raw: encodedMessage,
     },
-  }).then((res) => {
-    console.log(res);
-    return res;
-  });
-}
-
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  await login(page);
-  await page.screenshot({path: 'afterlogin.png'});
-
-  const unreadMessages = await getUnreadMessages(page);
-  for await (unreadMessage of unreadMessages) {
-    console.log(unreadMessage)
-    await message2mail(page, unreadMessage.href);
-    return
-  }
-
-  await browser.close();
-})();
-
-// https://developers.google.com/gmail/api/quickstart/nodejs#step_3_set_up_the_sample
-// https://github.com/googleapis/google-api-nodejs-client/blob/master/samples/gmail/send.js
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-    client_id, client_secret, redirect_uris[0]);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
   });
 }
 
@@ -169,7 +125,23 @@ function getNewToken(oAuth2Client, callback) {
         if (err) return console.error(err);
         console.log('Token stored to', TOKEN_PATH);
       });
-      callback(oAuth2Client);
+      auth = oAuth2Client;
     });
   });
 }
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await login(page);
+  await page.screenshot({path: 'afterlogin.png'});
+
+  const unreadMessages = await getUnreadMessages(page);
+  for await (unreadMessage of unreadMessages) {
+    await message2mail(page, unreadMessage.href);
+  }
+
+  await browser.close();
+})();
+
