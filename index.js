c9a7d69c99e4eb161832e657f528c9f1b400af56
shinespark <shnsprk@gmail.com>
2019-11-28 01:21:32 +0900

    取り敢えずメールもindex.jsから送信できたところまで

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
+const fs = require('fs');
 const moment = require('moment');
 const puppeteer = require('puppeteer');
+const readline = require('readline');
+const {google} = require('googleapis');
+const {promisify} = require('util');
 
-const sbisec = {
+const SBISEC = {
   "loginUrl": "https://site1.sbisec.co.jp/ETGate/",
   "messageBoxUrl": "https://site1.sbisec.co.jp/ETGate/?_ControlID=WPLETmbR001Control&_PageID=WPLETmbR001Rsub15&_DataStoreID=DSWPLETmbR001Control&_ActionID=DefaultAID&category_id=03&message_filter_value=2"
 }
 
+const SCOPES = ['https://www.googleapis.com/auth/gmail.send'];
+const TOKEN_PATH = 'token.json';
+let auth;
+fs.readFile('credentials.json', (err, content) => {
+  console.log(JSON.parse(content));
+  if (err) return console.log('Error loading client secret file:', err);
+  const {client_secret, client_id, redirect_uris} = JSON.parse(content).installed;
+  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    console.log(oAuth2Client);
+    // callback(oAuth2Client, from, to, title, body);
+    // callback(oAuth2Client);
+    auth = oAuth2Client;
+  });
+});
+
+
 const date = moment().format(moment.HTML5_FMT.DATETIME_LOCAL_MS);
 
 async function login(page) {
-  await page.goto(sbisec.loginUrl, {waitUntil: 'networkidle2'});
+  await page.goto(SBISEC.loginUrl, {waitUntil: 'networkidle2'});
 
   await page.type('#user_input > input', `${process.env.SBISEC_USERNAME}`);
   await page.type('#password_input > input', `${process.env.SBISEC_PASSWORD}`);
+  await page.screenshot({path: 'beforelogin.png'});
   await page.click('.ov'); // login button
 }
 
 async function getUnreadMessages(page) {
-  await page.goto(sbisec.messageBoxUrl, {waitUntil: 'networkidle2'});
+  await page.goto(SBISEC.messageBoxUrl, {waitUntil: 'networkidle2'});
 
   const unreadMessagesSelector = 'form > table:nth-of-type(2) table[width="100%"] a';
   const result = await page.$$eval(unreadMessagesSelector, unreadMessages => {
@@ -34,15 +60,116 @@ async function getUnreadMessages(page) {
   return result
 }
 
+// async function message2mail(page, url, oAuth2Client) {
+async function message2mail(page, url) {
+  await page.goto(url, {waitUntil: 'networkidle2'});
+
+  const subjectSelector = 'form > table:nth-of-type(2)';
+  const bodySelector = 'form > table:nth-of-type(3)';
+  const submitSelector = 'form > table:nth-of-type(4) input[type="submit"]';
+  const subject = await page.$eval(subjectSelector, item => {
+    return item.textContent;
+  });
+  const body = await page.$eval(bodySelector, item => {
+    return item.textContent;
+  });
+
+  // const res = await sendMail(oAuth2Client, subject, body);
+  const res = await sendMail(subject, body);
+  await console.log(res);
+  if (res && res.status === 200) {
+    // await page.click(submitSelector);
+  }
+}
+
+async function sendMail(subject, body) {
+  console.log(subject);
+  const utf8Subject = `=?utf-8?B?${Buffer.from(subject).toString('base64')}?=`;
+  const messageParts = [
+    `From: sbisec-message2mail <${process.env.FROM}>`,
+    `To: my own <${process.env.TO}>`,
+    'Content-Type: text/html; charset=utf-8',
+    'Content-Transfer-Encoding: 7bit',
+    'MIME-Version: 1.0',
+    `Subject: ${utf8Subject}`,
+    '',
+    `${body}`
+  ];
+  const message = messageParts.join('\n');
+
+  // The body needs to be base64url encoded.
+  const encodedMessage = Buffer.from(message)
+    .toString('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '');
+
+  const gmail = google.gmail({version: 'v1', auth: auth});
+  console.log(gmail)
+  gmail.users.messages.send({
+    userId: 'me',
+    requestBody: {
+      raw: encodedMessage,
+    },
+  }).then((res) => {
+    console.log(res);
+    return res;
+  });
+}
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   await login(page);
   await page.screenshot({path: 'afterlogin.png'});
+
   const unreadMessages = await getUnreadMessages(page);
-  console.log(unreadMessages);
-  // await captureAllPortfolio(page, 'service/portfolio', '.section-frame');
+  for await (unreadMessage of unreadMessages) {
+    console.log(unreadMessage)
+    await message2mail(page, unreadMessage.href);
+    return
+  }
 
   await browser.close();
 })();
+
+// https://developers.google.com/gmail/api/quickstart/nodejs#step_3_set_up_the_sample
+// https://github.com/googleapis/google-api-nodejs-client/blob/master/samples/gmail/send.js
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
