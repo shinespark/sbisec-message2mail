83ebe20d9d72bfb6bc5913123b50f3d0ca19baa6
shinespark <shnsprk@gmail.com>
2020-11-26 19:47:52 +0900

    enable eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: [
+    "eslint:recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  rules: {
+    indent: ["error", 2]
+  }
+}
